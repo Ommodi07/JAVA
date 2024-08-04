abab9df075ad470ce7bcf4fac806cae9b6beea59
Ommodi07 <ommodi0207@gmail.com>
2024-08-04 21:36:04 +0530

    q3

---
+++ Q-3/Student.java
@@ -0,0 +1,22 @@
+package Student_pack;
+
+public class Student {
+    private int studentID;
+    private String name;
+
+    public Student(int studentID, String name)
+    {
+        this.studentID = studentID;
+        this.name = name;
+    }
+
+    public int getStudentID()
+    {
+        return studentID;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
